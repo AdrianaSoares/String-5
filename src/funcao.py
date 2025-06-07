f96411695e92e04c656fee1af7b00c0ffbf52545
DriKa <adrianaapsoares@yahoo.com.br>
2025-06-07 01:33:53 +0100

    modificações

--- src/funcao.py
+++ src/funcao.py
@@ -2,12 +2,14 @@ import pandas as pd
 
 
 def dropar_na(df, coluna, limite_dados_faltantes):
+    print('olá')
     if df[coluna].isna().mean() > limite_dados_faltantes:
         df = df.dropna(subset=[coluna])
     return df
 
 
 def limpar_outlier(df, coluna):
+    print('olá')
     media = df[coluna].mean()  # calculo da média
     dp = df[coluna].std()  # desvio padrão
     # limite superior (como não é uma curva normal fez a multiplicação por 2)
